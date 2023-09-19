6060d4a033b09a91dde402722e3d162a6859c788
Rodrigo Brasil <108822086+Rbrasil72@users.noreply.github.com>
2023-09-19 15:24:32 +0000

    Update copysyslog.sh

--- copysyslog.sh
+++ copysyslog.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-#Diretorio + copia do ficheiro com
+#Diretorio + copia do ficheiro com a data do dia atual
 cp /var/log/syslog "syslog_$(date '+"%d-%m-%Y"').log"
 
+#Mensagem apresentada na terminal ao utilizador
 echo "File syslog_$(date '+"%d-%m-%Y"').log has been created!"
